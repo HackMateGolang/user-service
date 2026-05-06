06592da4502922f0b25f9ecf11952f6791a9f6de
Oleg_Markov (DxstW1sh) <gkdestroy@yandex.ru>
2026-05-07 00:36:13 +0300

    realize createUser and UserCaching functions

--- go.mod
+++ go.mod
@@ -12,5 +12,6 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
-	golang.org/x/text v0.20.0 // indirect
+	golang.org/x/sys v0.43.0 // indirect
+	golang.org/x/text v0.36.0 // indirect
 )
--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -1,6 +1,9 @@
 package repository
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/HackMateGolang/user-service/internal/models"
 	"github.com/redis/go-redis/v9"
 	"gorm.io/gorm"
@@ -15,9 +18,12 @@ func NewUserRepository(db *gorm.DB, redisClient *redis.Client) *userRepository {
 	return &userRepository{db: db, redisClient: redisClient}
 }
 
-func (r *userRepository) CreateUser (req *models.CreateUserRequest) (string, error) {
-	var login string
-	return login, nil
+func (r *userRepository) CreateUser (ctx context.Context, user *models.User) (string, error) {
+	if err := r.db.Model(&models.User{}).Create(user).Error; err != nil {
+		return "", fmt.Errorf("Repo: Create user failed: %w", err)
+	}
+
+	return user.Login, r.userCaching(ctx, user)
 }
 
 func (r *userRepository) ReadUser (req *models.ReadUserRequest) (string, error) {
@@ -40,3 +46,14 @@ func (r *userRepository) DeleteUser (req *models.DeleteUserRequest) (string, err
 	return login, nil
 }
 
+func (r *userRepository) userCaching(ctx context.Context, user *models.User) error {
+	key := userCacheKey(user.Login)
+	if err := r.redisClient.HSet(ctx, key, user).Err(); err != nil {
+		return fmt.Errorf("Repo: user caching failed: %w", err)
+	}
+	return nil
+}
+
+func userCacheKey(login string) string {
+	return fmt.Sprintf("user:%v", login)
+}
